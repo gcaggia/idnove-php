238fca567337918fe1af08fdf3cce30a941b189a
Guillaume Caggia <guillaume.caggia@gmail.com>
2015-11-07 17:49:13 +0100

    test git sublime

--- controller/connexion.php
+++ controller/connexion.php
@@ -1,5 +1,10 @@
 <?php
 
+if (isset($_SESSION['utilisateur'])) {
+	//redirection to account page
+    header('location: account.php');
+}
+
 require_once $_SERVER['DOCUMENT_ROOT'] . 'idnove/model/connexion.php';
 
 require_once $_SERVER['DOCUMENT_ROOT'] . 'idnove/view/connexion.php';
